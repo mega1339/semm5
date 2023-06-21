8a3bce35820d6218629bb200c6ba6526cf7669d4
mega1339 <119122112+mega1339@users.noreply.github.com>
2023-06-21 16:16:27 +0300

    Update 5.1.c

--- 5.1.c
+++ 5.1.c
@@ -5,13 +5,7 @@ int seArchFristSymbol(char s1[], char s2[], int length){
     for (int i = 0; i <= length; i++){
         for (int j = 0; j <= length; j++){
             if(s1[j] == s2[i])
-            {
                 return j;
-            }
-            if (j=length)
-            {
-                return -1;
-            }
         }
     }
     return -1;
